cb350ae0a830952fcd48cd987206a10ec700c64b
joshua1b <joshua1b2823@gmail.com>
2017-10-08 21:28:46 +0900

    add config template file for db connection.

---
+++ config-template.py
@@ -0,0 +1,8 @@
+# This is a template for main.py.
+# You should rename to config.py, and modify this as you want.
+
+DB_NAME = "sample_db"
+USER = "sample_user"
+PASSWORD = "sample_password"
+HOST = "localhost"
+PORT = "5432"
