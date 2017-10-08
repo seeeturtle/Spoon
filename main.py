cb547f698b87971fe4c0da29d1aca55f68c61afe
joshua1b <joshua1b2823@gmail.com>
2017-10-08 21:27:34 +0900

    add function saving data to db.

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from pprint import pprint
 from bs4 import BeautifulSoup
+import psycopg2
 import requests
 import datetime
 import re
+import config
 
 def main():
     time = datetime.datetime.now()
     foods = get_foods(time) # foods is a list of tuples contains food name and food stuff.
-    pprint(foods)
+    save_db(foods, time)
 
 def get_foods(time):
     year = time.year
     month = format(time.month, '02d')
     url = 'http://stu.goe.go.kr/sts_sci_md00_001.do?schulCode=J100006438&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=1&schYm=' + str(year) + str(month)
     req = requests.get(url)
-    return parse_foods(req.text)
+    return parse_foods(req.text, time)
 
-def parse_foods(text):
+def parse_foods(text, time):
     soup = BeautifulSoup(text, 'html.parser')
     table = soup.find_all(summary="이 표는 월간 식단에 관한 달력 정보를 제공하고 있습니다.")[0]
     tbody = table.find_all("tbody")[0]
@@ -28,19 +30,59 @@ def parse_foods(text):
         if txt != ' ':
             splitted = txt.split('[중식]')
             if len(splitted) > 1:
-                parsed.append((int(splitted[0]), splitted[1].replace('s', '')))
-    return foods_from_parsed(parsed)
+                parsed.append((str(format(int(splitted[0]), '02d')), splitted[1].replace('s', '')))
+    return foods_from_parsed(parsed, time)
 
-def foods_from_parsed(parsed):
+def foods_from_parsed(parsed, time):
     foods = []
-    for _, foodinfo in parsed:
+    for date, foodinfo in parsed:
         a = [m.group(0) for m in re.finditer('[^0-9]+([0-9]+[.])*', foodinfo)]
+        a_food = [time.strftime("%Y%m")+date]
         for x in a:
             food = re.match('[가-힣]+', x).group(0)
             pattern = re.compile('([0-9]+[.])+')
             foodstuff = tuple(int(x) for x in pattern.search(x).group(0)[:-1].split('.')) if pattern.search(x) else None
-            foods.append((food, foodstuff))
+            a_food.append((food, foodstuff))
+        foods.append(a_food)
     return foods
+
+def save_db(foods, time):
+    conn = psycopg2.connect(dbname=config.DB_NAME, user=config.USER, password=config.PASSWORD, \
+            host=config.HOST, port=config.PORT)
+    cur = conn.cursor()
+    for a_food in foods:
+        date = a_food[0]
+        cur.execute("select exists(select 1 from lunches where date = %s)", (date,))
+        has_data = cur.fetchone()[0]
+        if has_data:
+            continue
+        cur.execute("insert into lunches (date) values (%s) returning lunch_id" , (date,))
+        lunch_id = cur.fetchone()[0]
+        for food, foodstuff in a_food[1:]:
+            cur.execute("select exists(select 1 from foods where food_name = %s)", (food,))
+            has_food = cur.fetchone()[0]
+            if has_food:
+                cur.execute("select food_id from foods where food_name = %s", (food,))
+                food_id = cur.fetchone()[0]
+            else:
+                if foodstuff:
+                    cur.execute("insert into foods (food_name, delicious) values (%s, %s) returning food_id",
+                            (food, is_delicous(foodstuff)))
+                    food_id = cur.fetchone()[0]
+            if foodstuff is not None:
+                for fs in foodstuff:
+                    cur.execute("insert into foods_foodstuffs (food_id, foodstuff_id) values (%s, %s)", (food_id, fs))
+            cur.execute("insert into lunches_foods (lunch_id, food_id) values (%s, %s)", (lunch_id, food_id))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def is_delicous(foodstuff):
+    if foodstuff is not None:
+        if 5 in foodstuff and 13 in foodstuff:
+            return 't'
+        return 'f'
+    return None
 
 if __name__ == '__main__':
     main()
