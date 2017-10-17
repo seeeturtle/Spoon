20c355955d6ee4702b4ec7a6b02c1b569a3a2d69
joshua1b <joshua1b2823@gmail.com>
2017-10-17 21:42:02 +0900

    solve #1. touched code for solving.

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import psycopg2
 import requests
 import datetime
 import re
+import sys
 import config
+
+class Food:
+    def __init__(self, name=None, stuffs=None):
+        self.name = name
+        self.stuffs = stuffs
 
 def main():
     time = datetime.datetime.now()
-    foods = get_foods(time) # foods is a list of tuples contains food name and food stuff.
-    save_db(foods, time)
+    parsed = get_foods(time)
+    save_db(parsed, time)
 
 def get_foods(time):
     year = time.year
@@ -24,53 +30,66 @@ def parse_foods(text, time):
     tbody = table.find_all("tbody")[0]
     tds = tbody.find_all("td")
     divs = [td.find("div") for td in tds]
-    txts = [div.text for div in divs]
-    parsed = []
-    for txt in txts:
-        if txt != ' ':
-            splitted = txt.split('[중식]')
+    parsed = [] # type: List[str, List[Food]]
+    for div in divs:
+        txt = str(div)[5:-6].replace('[중식]<br/>', '').split('<br/>')
+        if len(txt) < 3:
+            continue
+        date = txt[0]
+        raw_food = txt[1:]
+        foods = []
+        for rf in raw_food:
+            food = Food()
+            splitted = [s for s in re.split('ss|s', rf) if s]
+            stuffs = []
+            m = re.search('([.]?[0-9]+[.]?)+', splitted[0])
+            # check if numbers are in name
+            if m:
+                raw_foodstuffs = m.group(0)
+                stuffs += [int(f) for f in raw_foodstuffs.split('.') if f]
+                food.name = splitted[0].replace(raw_foodstuffs, '')
+            else:
+                food.name = splitted[0]
+            # swap &amp; to &
+            food.name = food.name.replace('&amp;', '&')
             if len(splitted) > 1:
-                parsed.append((str(format(int(splitted[0]), '02d')), splitted[1].replace('s', '')))
-    return foods_from_parsed(parsed, time)
+                try:
+                    raw_foodstuffs = ''
+                    for c in splitted[1]:
+                        if c == '.' or c.isdigit():
+                            raw_foodstuffs += c
+                    stuffs += [int(s) for s in raw_foodstuffs.split('.') if s]
+                except:
+                    print(rf)
+            food.stuffs = stuffs
+            foods.append(food)
+        parsed.append((date, foods))
+    return parsed
 
-def foods_from_parsed(parsed, time):
-    foods = []
-    for date, foodinfo in parsed:
-        a = [m.group(0) for m in re.finditer('[^0-9]+([0-9]+[.])*', foodinfo)]
-        a_food = [time.strftime("%Y%m")+date]
-        for x in a:
-            food = re.match('[가-힣]+', x).group(0)
-            pattern = re.compile('([0-9]+[.])+')
-            foodstuff = tuple(int(x) for x in pattern.search(x).group(0)[:-1].split('.')) if pattern.search(x) else None
-            a_food.append((food, foodstuff))
-        foods.append(a_food)
-    return foods
-
-def save_db(foods, time):
+def save_db(parsed, time):
     conn = psycopg2.connect(dbname=config.DB_NAME, user=config.USER, password=config.PASSWORD, \
             host=config.HOST, port=config.PORT)
     cur = conn.cursor()
-    for a_food in foods:
-        date = a_food[0]
+    for lunch in parsed:
+        date = time.strftime('%Y%m') + '{:02d}'.format(int(lunch[0]))
         cur.execute("select exists(select 1 from lunches where date = %s)", (date,))
         has_data = cur.fetchone()[0]
         if has_data:
             continue
         cur.execute("insert into lunches (date) values (%s) returning lunch_id" , (date,))
         lunch_id = cur.fetchone()[0]
-        for food, foodstuff in a_food[1:]:
-            cur.execute("select exists(select 1 from foods where food_name = %s)", (food,))
+        for food in lunch[1]:
+            cur.execute("select exists(select 1 from foods where food_name = %s)", (food.name,))
             has_food = cur.fetchone()[0]
             if has_food:
-                cur.execute("select food_id from foods where food_name = %s", (food,))
-                food_id = cur.fetchone()[0]
+                cur.execute("select food_id from foods where food_name = %s",
+                        (food.name,))
             else:
-                if foodstuff:
-                    cur.execute("insert into foods (food_name, delicious) values (%s, %s) returning food_id",
-                            (food, is_delicous(foodstuff)))
-                    food_id = cur.fetchone()[0]
-            if foodstuff is not None:
-                for fs in foodstuff:
+                cur.execute("insert into foods (food_name, delicious) values (%s, %s) returning food_id",
+                        (food.name, is_delicous(food.stuffs)))
+            food_id = cur.fetchone()[0]
+            if food.stuffs:
+                for fs in food.stuffs:
                     cur.execute("insert into foods_foodstuffs (food_id, foodstuff_id) values (%s, %s)", (food_id, fs))
             cur.execute("insert into lunches_foods (lunch_id, food_id) values (%s, %s)", (lunch_id, food_id))
     conn.commit()
@@ -78,7 +97,7 @@ def save_db(foods, time):
     conn.close()
 
 def is_delicous(foodstuff):
-    if foodstuff is not None:
+    if foodstuff:
         if 5 in foodstuff and 13 in foodstuff:
             return 't'
         return 'f'
