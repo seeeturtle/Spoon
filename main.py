8a46f9d3a4cb2e1b512b485796ed7696c4756509
joshua1b <joshua1b2823@gmail.com>
2017-11-04 16:18:27 +0900

    Corrected accident for leaving code for debugging.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import datetime
 import parse
 import sys
-import pprint
 
 def main():
     year, month, day = sys.argv[1:]
     time = datetime.datetime(int(year), int(month), int(day))
     parsed = parse.get_foods(time)
-    pprint.pprint(parsed)
-    # parse.save_db(parsed, time)
+    parse.save_db(parsed, time)
 
 
 if __name__ == '__main__':
